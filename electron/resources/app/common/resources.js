a7abce9da3eeae2d9b6437830e005bdc4e44da42
liudong <1137293945@qq.com>
2017-04-27 14:33:30 +0800

    555

---
+++ electron/resources/app/common/resources.js
@@ -0,0 +1,10 @@
+var sku;
+(function (sku) {
+    (function (spread) {
+        (function (designer) {
+            designer.res;
+        })(spread.designer || (spread.designer = {}));
+        var designer = spread.designer;
+    })(sku.spread || (sku.spread = {}));
+    var spread = sku.spread;
+})(sku || (sku = {}));
